975d802186d608557a1e025d730a64791a0c2047
Saketh Ayyagari <sakethsarma07@gmail.com>
2023-11-03 12:52:34 -0400

    ...

--- QuickSort1/LomutoAlgorithm.java
+++ QuickSort1/LomutoAlgorithm.java
@@ -1,52 +1,60 @@
 import java.io.*;
 import java.util.Scanner;
 /*
-Saketh Ayyagari
-Implementation of QuickSort using Lomuto's Partition
+Saketh Ayyagari, 3nov2023
+Implementation of QuickSort using Lomuto's Partitioning Scheme
 */
 public class LomutoAlgorithm{
-	static int[] getData(){
+	static String[] getData(){ 
 		Scanner sc = new Scanner(System.in);
-		int[] temp = new int[1000];
+		String[] temp = new String[1000];
 		int x = 0;
 		while (sc.hasNextLine()){
-			temp[x] = sc.nextInt();
+			temp[x] = sc.nextLine();
+			x+=1;
 		}
-		int[] output = new int[x];
+		String[] output = new String[x]; // input with a more fixed 
 		for (int i = 0; i < output.length; i+=1){
 			output[i] = temp[i];
 		}
 		return output;
 	}
-	public static void exchange(int arr[], int a, int b){ //swaps two elements in an array given their indecies
-		int temp = arr[a];
-		arr[a] = arr[b];
-		arr[b] = temp;
-	}
-	static void partition(int a[], int m, int h){ //sorts all elements between the lower bound m and upper bound h
-		int pivot = m; //index of pivot element
-		int r = m + 1;
-		if (h - m + 1 > 2){
-			while (r < h-1){
-				if (a[r] < a[pivot]){
+	public static void partition(String a[], int l, int h){ 
+		//sorts the array from the lower bound l (which doubles as the pivot) and the upper bound h inclusive
+		if (h - l > 0){
+			String temp = ""; //temporary variable used for exchanging elements
+			int m = l; //keeps track of the element being swapped when it is less than the pivot 
+			int r = l + 1;
+			while (r <= h){ //r index traverses up until the upper bound
+				if (a[r].compareTo(a[l]) < 0){
 					m+=1;
-					exchange(a, m, r);
+					//does the exchange of elements
+					temp = a[m];
+					a[m] = a[r];
+					a[r] = temp;	
 				}
 				r+=1;
 			}
+			//exchanges pivot with element at index m
+			temp = a[l];
+			a[l] = a[m];
+			a[m] = temp;
+			
+			partition(a, l, m-1); //partitions lower half
+			partition(a, m+1, h); //partitions upper half
 		}
-		exchange(a, pivot, m);
-		partition(a, 0, pivot);
-		partition(a, pivot+1, h-1);
 	}
-	public static void sort(int arr[]){ //sorts the entire array
-		partition(arr, 0, arr.length);
+	public static void sort (String a[], int l, int h){ //sorts entire array
+		partition(a, 0, a.length-1);
+	}
+	public static void printData(String[] a){
+		for (int i = 0; i < a.length; i+=1){
+			System.out.println(a[i]);
+		}
 	}
 	public static void main(String[] args) {
-		int[] array = getData();
-		sort(array);
-		for (int i = 0; i < array.length; i+=1){
-			System.out.println(array[i]);
-		}
+		String[] array = getData();
+		sort(array, 0, array.length-1);
+		printData(array);
 	}
 }
