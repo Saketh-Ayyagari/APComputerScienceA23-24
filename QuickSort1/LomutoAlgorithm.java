d63d2351b4dc2622dbd0ecf1f176a690564f3b36
Saketh Ayyagari <sakethsarma07@gmail.com>
2023-11-02 20:57:42 -0400

    Final quicksort base model

--- QuickSort1/LomutoAlgorithm.java
+++ QuickSort1/LomutoAlgorithm.java
@@ -19,35 +19,37 @@ public class LomutoAlgorithm{
 		}
 		return output;
 	}
-	public static void partition(String a[], int l, int h){ 
+	public static int partition(String a[], int l, int h){ 
 		//sorts all elements between the lower bound m and upper bound h inclusive
-		if (l < h){ // is the size of the subarray being partitioned at least 1?
-			String temp = ""; //temporary variable used for exchanging elements
-			int m = l; //counter that keeps track of the index being swapped
-			int pivot = l; 
-			int r = l + 1;
 		
-			while (r <= h){ //r index traverses up until the upper bound
-				if (a[r].compareTo(a[pivot]) < 0){
-					m+=1;
-					//does the exchange of elements
-					temp = a[m];
-					a[m] = a[r];
-					a[r] = temp;	
-				}
-				r+=1;
+		String temp = ""; //temporary variable used for exchanging elements
+		int m = l; //counter that keeps track of the index being swapped
+		int pivot = l; 
+		int r = l + 1;
+	
+		while (r <= h){ //r index traverses up until the upper bound
+			if (a[r].compareTo(a[pivot]) < 0){
+				m+=1;
+				//does the exchange of elements
+				temp = a[m];
+				a[m] = a[r];
+				a[r] = temp;	
 			}
-			//exchanges pivot with element at index m
-			temp = a[pivot];
-			a[pivot] = a[m];
-			a[m] = temp;
-			
-			partition(a, l, pivot-1); //partition for lower half
-			partition(a, pivot+1, h); //partition for upper half
+			r+=1;
 		}
+		//exchanges pivot with element at index m
+		temp = a[pivot];
+		a[pivot] = a[m];
+		a[m] = temp;
+		
+		return m;
 	}
-	public static void sort(String arr[]){ //sorts the entire array
-		partition(arr, 0, arr.length-1);
+	public static void qSort (String a[], int l, int h){
+		if (l <= h){ // is the size of the subarray at least 1?
+			int pivot = partition(a, l, h);
+			qSort(a, l, pivot-1); //partition for lower half
+			qSort(a, pivot+1, h); //partition for upper half
+		}
 	}
 	public static void printData(String[] a){
 		for (int i = 0; i < a.length; i+=1){
@@ -56,7 +58,7 @@ public class LomutoAlgorithm{
 	}
 	public static void main(String[] args) {
 		String[] array = getData();
-		sort(array);
+		qSort(array, 0, array.length-1);
 		printData(array);
 	}
 }
